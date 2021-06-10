f5afc78a465a4ec1d9308193c29e24b41139ca88
Погосян Артем Валерьевич <avpogosyan@vtb.ru>
2021-06-10 16:29:23 +0300

    added repository annotation

--- src/main/java/ru/geekbrains/spring/first/app/repositories/OrdersRepository.java
+++ src/main/java/ru/geekbrains/spring/first/app/repositories/OrdersRepository.java
@@ -1,9 +1,9 @@
 package ru.geekbrains.spring.first.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.geekbrains.spring.first.app.model.Order;
 
-@Component
+@Repository
 public interface OrdersRepository extends JpaRepository<Order, Long> {
 }
